c338b68ee73aefafbb3b0e2c0f2167ed9cbe254a
rjspyk5 <rjspyk5@gmail.com>
2024-07-10 19:21:14 +0600

    simple react tabs added and installed matariel ui

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import { AboutTab } from "../../components/AboutTab/AboutTab";
 import { Banner } from "../../components/Banner/Banner";
 import { Contact } from "../../components/Contact/Contact";
 import { Education } from "../../components/Education/Education";
 import { ProjectSection } from "../../components/ProjectSection/ProjectSection";
 import { SkillSection } from "../../components/SkillSection/SkillSection";
+
 import { SectionHeading } from "./../../components/sectionHeading/SectionHeading";
 export const Home = () => {
   return (
@@ -14,6 +16,7 @@ export const Home = () => {
       <SkillSection />
 
       <ProjectSection />
+      <AboutTab />
 
       <Contact />
     </div>
---
+++ src/components/AboutTab/AboutTab.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+
+export const AboutTab = () => {
+  return (
+    <div>
+      <Tabs>
+        <TabList>
+          <div className="flex">
+            <Tab>Title 1</Tab>
+            <Tab>Title 2</Tab>
+          </div>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+};
